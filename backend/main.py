e4190c08d6930cd379c5a3fa12120073f28d9f8a
brownhat16 <118588843+brownhat16@users.noreply.github.com>
2025-05-15 17:43:34 +0000

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -317,4 +317,4 @@ async def compare_images(files: List[UploadFile] = File(...), categories: str = 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
